a1a067f3442d7ae8b38b0b1971568929fc119925
devbyte <demiladeakinyemi2004@gmail.com>
2026-02-12 12:05:34 -0500

    feat: implement Stay Discovery MVP for Arrivo assessment

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,4 +1,4 @@
-export { Button, type ButtonProps } from "./button";
+export { Button } from "./button";
 export { Input, type InputProps } from "./input";
 export { Badge, type BadgeProps } from "./badge";
 export {
